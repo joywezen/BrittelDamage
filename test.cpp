48f65adc7138794c38931c596863efdaeaa2aeed
joywezen <joywezen@gmail.com>
2018-12-06 22:59:55 +0100

    a cpp test including solving display problem using xterm

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "I have now bigger font size, Oh Yeah!";
+    return 0;
+    /* XTerm*faceName: Ubuntu Mono:style=Regular
+    xrdb -merge .Xresources
+    xterm
+    */
+}
